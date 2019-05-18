09f9747620ce55cfc25f1266aff3c35cef9ca713
rlaehdgus27 <kdh9183@naver.com>
2019-05-18 19:02:19 +0900

    First Commit

---
+++ machine.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>자판기</title>
+    </head>    
+        <body>
+            <h1>자 판 기</h1>
+            <p>무엇을 고르시겠습니까?</p>
+            <br>
+            <p>
+                <button>콜라</button>
+                <button>사이다</button>
+                <button>파워에이드</button>
+                <button>커피</button>
+                <button>우유</button>
+                <button>율무차1</button>
+            </p>
+            <br>
+            돈넣는곳<input>
+            
+        </body>
+</html>
