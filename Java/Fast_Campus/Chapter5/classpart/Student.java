0fa5dd239d69a271e77af7fccba8228bcdf5e577
DEV_KDH <kdh9183@naver.com>
2019-11-29 01:11:24 +0900

    FastCampus Chapter5 classpart

---
+++ Java/Fast_Campus/Chapter5/classpart/Student.java
@@ -0,0 +1,14 @@
+package classpart;
+
+public class Student {
+
+		int 	studentID;
+		String 	studentName;
+		String	address;
+		
+		public void showStudentInfo() {
+			System.out.println(studentName + "," + address);
+		}
+	}
+
+
---
+++ Java/Fast_Campus/Chapter5/classpart/StudentTest.java
@@ -0,0 +1,14 @@
+package classpart;
+
+public class StudentTest {
+	
+	public static void main(String[] args) {
+		
+		Student studentKim = new Student();
+		studentKim.studentName	= "±èµ¿Çö";
+		studentKim.address		= "´ëÀü";
+		
+		studentKim.showStudentInfo();
+	}
+
+}
